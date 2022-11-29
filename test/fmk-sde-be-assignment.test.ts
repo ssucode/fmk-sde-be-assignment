843b14395f707dabfb0e6dd78c4beebd098db3b0
ssucode <ssuya79@gmail.com>
2022-11-30 01:31:49 +0900

    chore: add test to create paticipants table

--- test/fmk-sde-be-assignment.test.ts
+++ test/fmk-sde-be-assignment.test.ts
@@ -1,27 +1,39 @@
 import { Template } from 'aws-cdk-lib/assertions';
 import { App } from 'aws-cdk-lib';
-import {FmkSdeBeAssignmentStack} from "../lib/fmk-sde-be-assignment-stack";
+import { FmkSdeBeAssignmentStack } from '../lib/fmk-sde-be-assignment-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/fmk-sde-be-assignment-stack.ts
 describe('FMK Sde Be Assignment Stack', () => {
-    let app: App;
-    let stack: FmkSdeBeAssignmentStack;
-    let template: Template;
+  let app: App;
+  let stack: FmkSdeBeAssignmentStack;
+  let template: Template;
 
-    beforeAll(() => {
-        app = new App();
-        stack = new FmkSdeBeAssignmentStack(app, 'TestStack');
-        template = Template.fromStack(stack);
-    });
+  beforeAll(() => {
+    app = new App();
+    stack = new FmkSdeBeAssignmentStack(app, 'TestStack');
+    template = Template.fromStack(stack);
+  });
 
-    it('should create lambda function', () => {
-        template.hasResourceProperties('AWS::Lambda::Function', {});
-    });
+  it('should create lambda function', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {});
+  });
 
-    it('should create a RestApi resource', () => {
-        template.hasResourceProperties('AWS::ApiGateway::RestApi', {
-            Name: 'Participation-API-Service',
-        });
+  it('should create a RestApi resource', () => {
+    template.hasResourceProperties('AWS::ApiGateway::RestApi', {
+      Name: 'Participation-API-Service',
     });
+  });
+
+  it('should create paticipants table', () => {
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+      TableName: 'paticipants',
+      KeySchema: [
+        {
+          AttributeName: 'email',
+          KeyType: 'HASH',
+        },
+      ],
+    });
+  });
 });
