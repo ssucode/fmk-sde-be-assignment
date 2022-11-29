66931fc85d31c8e7c37246d1bac0fe961bae39b6
ssucode <ssuya79@gmail.com>
2022-11-30 01:07:38 +0900

    feat: add Create DynamoDB and table stack, add lambda environment variable

---
+++ lib/dynamodb-construct.ts
@@ -0,0 +1,21 @@
+import { Construct } from 'constructs';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+
+export class DynamoDbConstruct extends Construct {
+  readonly paticipantsTable: Table;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // create paticipants table on DynamoDB
+    // eslint-disable-next-line no-new
+    this.paticipantsTable = new Table(this, 'paticipants-table', {
+      partitionKey: {
+        name: 'email',
+        type: AttributeType.STRING,
+      },
+      tableName: 'paticipants',
+      billingMode: BillingMode.PROVISIONED,
+    });
+  }
+}
