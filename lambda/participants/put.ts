5a8a0566928e6a3aec590822d0c12534e49aa62e
ssucode <ssuya79@gmail.com>
2022-11-30 15:14:49 +0900

    chore: paticipant put(upsert) lambda funtion additional implemented
    - add class-validator
    - add APIGatewayProxy Headers parameter
    - upsert function additional implemented

--- lambda/participants/put.ts
+++ lambda/participants/put.ts
@@ -1,8 +1,33 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { DynamoDB } from 'aws-sdk';
-import { ConditionalCheckFailedException } from '@aws-sdk/client-dynamodb';
-import { Converter, ParameterizedStatement } from 'aws-sdk/clients/dynamodb';
+import { AttributeMap, Converter } from 'aws-sdk/clients/dynamodb';
+import { IsEmail, IsOptional, IsString, IsBoolean, IsDate, validateOrReject, IsNotEmpty } from 'class-validator';
 
+class ParticipantDto {
+  @IsEmail()
+  @IsNotEmpty()
+  Email: string; // 이메일
+
+  @IsString()
+  @IsNotEmpty()
+  Name: string; // 국가 이름
+
+  @IsString()
+  @IsNotEmpty()
+  Mobile: string; // 국가코드 + 전화번호
+
+  @IsBoolean()
+  @IsNotEmpty()
+  Agree: boolean; // 개인정보 수집 동의 여부
+
+  @IsDate()
+  @IsOptional()
+  CreatedAt?: string; // 생성일
+
+  @IsDate()
+  @IsOptional()
+  LastUpdatedAt?: string; // 수정일
+}
 /**
  * interface
  * 변수를 정의하고 타입을 설정합니다
@@ -13,6 +38,7 @@ interface Participant {
   Mobile: string; // 국가코드 + 전화번호
   Agree: boolean; // 개인정보 수집 동의 여부
   CreatedAt?: string; // 생성일
+  LastUpdatedAt?: string; // 수정일
 }
 
 /**
@@ -58,6 +84,10 @@ const StatusCodeResult = (
   return <APIGatewayProxyResult>{
     statusCode,
     headers: {
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Origin': '*',
       ...(headers || {}),
     },
     body: jsonBody,
@@ -69,95 +99,109 @@ const StatusCodeResult = (
  * @param ddb
  * @param participant
  */
-const updateOrAdd = async (ddb: DynamoDB, participant: Participant): Promise<Participant | undefined> => {
-  let result: Participant | undefined;
+const upsert = async (ddb: DynamoDB, participant: Participant): Promise<Participant | undefined> => {
+  const participantDto = new ParticipantDto();
+  participantDto.Email = participant.Email;
+  participantDto.Name = participant.Name;
+  participantDto.Mobile = participant.Mobile;
+  participantDto.Agree = participant.Agree;
+
+  await validateOrReject(participantDto).catch((errors) => {
+    console.error('Caught promise rejection (validation failed), error: ', errors.errorMessage);
+    return undefined;
+  });
 
   try {
-    result = await update(ddb, participant);
-    if (result) {
-      // return if update is success
-      return result;
+    const data = await get(ddb, participant);
+    if (data) {
+      return await update(ddb, participant);
     }
+    return await insert(ddb, participant);
   } catch (error) {
-    if (!(error instanceof ConditionalCheckFailedException)) {
-      throw error;
-    }
+    console.error(`an error occurred while updating activities on the email: ${participant.Email}, error: `, error);
+    return undefined;
   }
-
-  return await add(ddb, participant);
 };
 
 /**
- * DynamoDB에 Insert합니다.
- * @param participant
+ * get DynamoDB에서 Email과 일치하는 정보를 리턴합니다.
+ * @param ddb DynamoDB 인스턴스
+ * @param email 검색할 Email
  */
-const createInsertStatement = (participant: Participant): ParameterizedStatement => {
-  return <ParameterizedStatement>{
-    Statement: `INSERT INTO paticipants VALUE {
-      'Email': ?,
-      'Name': ?,
-      'Mobile': ?,
-      'Agree': ?
-    }`,
-    Parameters: [participant.Email, participant.Name, participant.Mobile, participant.Agree].map((item: any) =>
-      Converter.input(item),
-    ),
-  };
+const get = async (ddb: DynamoDB, participant: Participant): Promise<Participant | undefined> => {
+  const { Items = [] } = await ddb
+    .executeStatement({
+      Statement: 'SELECT * FROM paticipants WHERE Email = ?',
+      Parameters: [{ S: participant.Email }],
+    })
+    .promise();
+
+  return Items.length === 0
+    ? undefined
+    : Items.map((item: AttributeMap) => Converter.unmarshall(item) as Participant).shift();
 };
 
 /**
  * Insert 처리를 합니다.
  * @param ddb
- * @param participantPayload
+ * @param participant
  */
-const add = async (ddb: DynamoDB, participantPayload: Participant): Promise<Participant | undefined> => {
-  const participant: Participant = {
-    ...participantPayload,
-    CreatedAt: new Date().toISOString(),
-  };
-
-  const statement = createInsertStatement(participant);
+const insert = async (ddb: DynamoDB, participant: Participant): Promise<Participant | undefined> => {
   try {
-    await ddb.executeStatement(statement).promise();
-  } catch (error) {
-    // somehow, error instanceof DuplicateItemException returns false
-    // so we need to check error.code to determine the type of exception.
-    if (error.code === 'DuplicateItemException') {
-      return undefined;
-    }
-    throw error;
-  }
+    const { Items = [] } = await ddb
+      .executeStatement({
+        Statement: `INSERT INTO paticipants VALUE {
+          'Email': ?,
+          'Name': ?,
+          'Mobile': ?,
+          'Agree': ?,
+          'CreatedAt': ?
+        }`,
+        Parameters: [
+          participant.Email,
+          participant.Name,
+          participant.Mobile,
+          participant.Agree,
+          new Date().toISOString(),
+        ].map((item: any) => Converter.input(item)),
+      })
+      .promise();
 
-  return participant;
+    return Items.map((item: AttributeMap) => Converter.unmarshall(item) as Participant).shift();
+  } catch (error) {
+    console.error('insert error!, error: ', error);
+    return undefined;
+  }
 };
 
 /**
  * Update 처리를 합니다.
- * @param ddb DynamoDB 인스턴스
+ * @param ddb
  * @param participant
  */
 const update = async (ddb: DynamoDB, participant: Participant): Promise<Participant | undefined> => {
-  if (!participant.Email) {
-    throw new Error('Email이 존재하지 않습니다');
+  const updateStatement =
+    'UPDATE paticipants SET Name = ?, Mobile = ?, Agree = ?, LastUpdatedAt = ? WHERE Email = ? RETURNING ALL NEW *';
+
+  try {
+    const { Items = [] } = await ddb
+      .executeStatement({
+        Statement: updateStatement,
+        Parameters: [
+          participant.Name,
+          participant.Mobile,
+          participant.Agree,
+          new Date().toISOString(),
+          participant.Email,
+        ].map((item: any) => Converter.input(item)),
+      })
+      .promise();
+
+    return Items.map((item: AttributeMap) => Converter.unmarshall(item) as Participant).shift();
+  } catch (error) {
+    console.error('update error!, error: ', error);
+    return undefined;
   }
-
-  const newParticipant = {
-    ...participant,
-  };
-
-  await ddb
-    .executeTransaction({
-      TransactStatements: [
-        {
-          Statement: 'DELETE FROM paticipants WHERE Id = ?',
-          Parameters: [Converter.input(participant.Email)],
-        },
-        createInsertStatement(newParticipant),
-      ],
-    })
-    .promise();
-
-  return newParticipant;
 };
 
 export const handler = async (request: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -173,7 +217,6 @@ export const handler = async (request: APIGatewayProxyEvent): Promise<APIGateway
   }
 
   const ddb = new DynamoDB();
-  const data = await updateOrAdd(ddb, payload);
-
+  const data = await upsert(ddb, payload);
   return OkResult(request, data);
 };
